88e12210943eaf27ec5456bcd0afb00953ddd397
Raul Alanis <rulosc07@gmail.com>
2016-12-26 10:56:47 -0600

    mensage de error al registarse

--- views/site/abrirSesion.php
+++ views/site/abrirSesion.php
@@ -11,9 +11,13 @@ $form = ActiveForm::begin ( [
 ?>
 	<label>Ingrese correo</label>
 	<input type="email" name="email-gerente">
+	<?php if($message == 0){?>
+		<p>El correo es incorrecto</p>
+	<?php }?>
 		
 	<?= Html::submitButton('<span class="ladda-label">Registrarse</span>', ['id'=>'sesion-btn','class'=>'btn btn-primary ladda-button', 'data-style'=>'zoom-out'])?>
-
+	<a class="btn btn-primary" href="<?= Yii::$app->urlManager->createAbsoluteUrl ( ['/site/registro-gerentes'] ) ?>">Registrarse como Gerente</a>
+	
 <?php
 ActiveForm::end ();
 ?>
