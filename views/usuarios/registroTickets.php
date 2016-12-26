5ecd141a488e847d6a4af401a2acaec76c25885f
damsSanch <damian2gom@gmail.com>
2016-12-26 12:17:28 -0600

    Guardar ticket u otro ticker

--- views/usuarios/registroTickets.php
+++ views/usuarios/registroTickets.php
@@ -3,24 +3,52 @@ use yii\widgets\ActiveForm;
 use yii\helpers\Html;
 use yii\helpers\ArrayHelper;
 use app\models\CatSucursal;
+use yii\helpers\Url;
 
-$form = ActiveForm::begin ( [
-		'options' => [
-				'enctype' => 'multipart/form-data'
-		],
-		'id' => 'from-ticket',
-		"action" => Yii::$app->urlManager->createAbsoluteUrl ( ['usuarios/guardar-relacion?id=' . $idUsuario] ),
-] );
 ?>	
 
-	<?= $form->field($ticket, 'id_cadena')->dropDownList(ArrayHelper::map($cadenas, 'id_cadena', 'txt_nombre'), ['prompt'=>'Seleciona una cadena','class' => 'js-id-cadena'])?>
+<!-- .ur4 -->
+<div class="ur4">
+
+	<a href="" class="logo">
+		<img src="<?=Url::base()?>/webAssets/images/logo.png" alt="Kotex">
+	</a>
+
+	<img src="<?=Url::base()?>/webAssets/images/avion.png" class="avion" alt="Avion">
+
+	<img src="<?=Url::base()?>/webAssets/images/productos.png" class="productos" alt="Productos">
+
+	<!-- .ur4-cont -->
+	<div class="ur4-cont">
+
+		<!-- <h2>Datos del Gerente</h2> -->
+
+		<?php
+		$form = ActiveForm::begin ( [
+			'options' => [
+					'enctype' => 'multipart/form-data'
+			],
+			'id' => 'from-ticket',
+			"action" => Yii::$app->urlManager->createAbsoluteUrl ( ['usuarios/guardar-relacion?id=' . $idUsuario] ),
+		] );
+		?>
+
+			<?= $form->field($ticket, 'id_cadena')->dropDownList(ArrayHelper::map($cadenas, 'id_cadena', 'txt_nombre'), ['prompt'=>'Seleciona una cadena','class' => 'js-id-cadena'])?>
 			
-	<?= $form->field($ticket, 'id_sucursal')->dropDownList(ArrayHelper::map(CatSucursal::find()->one(), 'id_sucursal', 'txt_nombre'),['prompt'=>'Seleciona una sucursal'])?>
+			<?= $form->field($ticket, 'id_sucursal')->dropDownList(ArrayHelper::map(CatSucursal::find()->one(), 'id_sucursal', 'txt_nombre'),['prompt'=>'Seleciona una sucursal'])?>
+					
+			<?= $form->field($ticket, 'txt_ticket')->textInput(['maxlength' => true])?>
+
+			<?= Html::submitButton('<span class="ladda-label">Registrar otro ticket</span>', ['id'=>'sesion-btn-ticket','class'=>'btn btn-primary ladda-button', 'data-style'=>'zoom-out'])?>
 			
-	<?= $form->field($ticket, 'txt_ticket')->textInput(['maxlength' => true])?>
-		
-	<?= Html::submitButton('<span class="ladda-label">Registrar otro ticket</span>', ['id'=>'sesion-btn-ticket','class'=>'btn btn-primary ladda-button', 'data-style'=>'zoom-out'])?>
-	<a id="terminar_ticket" class="btn btn-primary" href="<?= Yii::$app->urlManager->createAbsoluteUrl ( ['/site/registro'] ) ?>">Terminar</a>
-<?php
-ActiveForm::end ();
-?>
+			<a id="terminar_ticket" class="btn btn-primary" href="<?= Yii::$app->urlManager->createAbsoluteUrl ( ['/site/registro'] ) ?>">Terminar</a>
+
+		<?php
+		ActiveForm::end ();
+		?>
+
+	</div>
+	<!-- end - .ur4-cont -->
+
+</div>
+<!-- end - .ur4 -->
