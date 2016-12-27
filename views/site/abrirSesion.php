7e38a760bb6049057e67df038e2478f7d961f429
damsSanch <damian2gom@gmail.com>
2016-12-26 18:04:20 -0600

    Ingresar - Registrar gerentes (css-php)

--- views/site/abrirSesion.php
+++ views/site/abrirSesion.php
@@ -18,30 +18,40 @@ use yii\helpers\Url;
 	<!-- .mt2-cont -->
 	<div class="mt2-cont">
 	
-	<h2>Ingresar</h2>
+		<h2 class="ingresar">Ingresar</h2>
 
-<?php
-$form = ActiveForm::begin ( [
-		'options' => [
-				'enctype' => 'multipart/form-data'
-		],
-		"action" => Yii::$app->urlManager->createAbsoluteUrl ( ['site/sesion'] ),
-] );
-?>
+		<?php
+		$form = ActiveForm::begin ( [
+				'options' => [
+						'enctype' => 'multipart/form-data'
+				],
+				"action" => Yii::$app->urlManager->createAbsoluteUrl ( ['site/sesion'] ),
+		] );
+		?>
 
 
-<?= $form->field($gerente, 'txt_correo')->textInput(['maxlength' => true])?>
-	<?php if($message == 0){?>
-		<p>El correo es incorrecto</p>
-	<?php }?>
-		
-	<?= Html::submitButton('<span class="ladda-label">Ingresar</span>', ['id'=>'sesion-btn','class'=>'btn btn-primary ladda-button', 'data-style'=>'zoom-out'])?>
-	<a class="btn btn-primary ladda-button" data-style="zoom-out" href="<?= Yii::$app->urlManager->createAbsoluteUrl ( ['/site/registro-gerentes'] ) ?>"><span class="ladda-label">Registrarse</span></a>
-	
-<?php
-ActiveForm::end ();
-?>
-</div>
+		<?= $form->field($gerente, 'txt_correo')->textInput(['maxlength' => true])?>
+			<?php if($message == 0){?>
+				<div class="help-error">
+					<p>El correo es incorrecto</p>
+				</div>
+			<?php }?>
+
+			<!-- .form-group-botones -->
+			<div class="form-group-botones">
+				
+				<a class="btn btn-secundary ladda-button" data-style="zoom-out" href="<?= Yii::$app->urlManager->createAbsoluteUrl ( ['/site/registro-gerentes'] ) ?>"><span class="ladda-label">Registrarse</span></a>
+				
+				<?= Html::submitButton('<span class="ladda-label">Ingresar</span>', ['id'=>'sesion-btn','class'=>'btn btn-primary ladda-button', 'data-style'=>'zoom-out'])?>
+
+			</div>
+			<!-- end - .form-group-botones -->
+			
+		<?php
+		ActiveForm::end ();
+		?>
+
+	</div>
 	<!-- end - .mt2-cont -->
 
 </div>
