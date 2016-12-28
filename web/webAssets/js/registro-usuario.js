0d4d6ef0f1ec5963f9049ab33b2dd94994bc9f10
damsSanch <damian2gom@gmail.com>
2016-12-28 16:13:15 -0600

    Modal aviso de privacidad

--- web/webAssets/js/registro-usuario.js
+++ web/webAssets/js/registro-usuario.js
@@ -1,3 +1,24 @@
+// Modal
+var modal = document.getElementById('modal-agregar-post');
+var modalOpen = document.getElementById("modal-agregar-post-open");
+var modalClose = document.getElementById("modal-agregar-post-close");
+
+$(document).ready(function(){
+	
+	/**
+	 * Modal
+	 */
+	// Open Modal
+	$(modalOpen).on("click", function(){
+		modal.style.display = "flex";
+	});
+	// Close Modal
+	$(modalClose).on("click", function(){
+		modal.style.display = "none";
+	});
+
+}); // end - READY
+
 $('body').on(
 		'beforeSubmit',
 		'#from-usuarios',
@@ -18,3 +39,40 @@ $('body').on(
 		 	l.start();
 			
 		});
+
+$(".mask-check").on("click", function(){
+
+	modal.style.display = "flex";
+
+});
+
+$("#modal-acepto-aviso-privacidad").on("click", function(){
+
+	$(".mask-check").hide();
+	$("#box-1").prop( "checked", true );
+	modal.style.display = "none";
+
+});
+
+$("#box-1").on("click", function(){
+	if($("#box-1").is(':checked')) {
+		// alert("Está activado");
+	} else {
+		$(".mask-check").show();
+	}
+});
+
+
+window.onclick = function(event) {
+	// Modal - Close
+	if (event.target == modal) {
+		modal.style.display = "none";
+	}
+}
+
+$(window).bind("click touchstart", function(){
+	// Modal - Close
+	if (event.target == modal) {
+		modal.style.display = "none";
+	}
+});
